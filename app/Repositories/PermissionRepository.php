3ed2af7e6536382a70b06ef2c8b68fb49141dec4
txrangel <joaovitorrtd@gmail.com>
2025-02-05 12:28:43 -0300

    feat: create repositories

---
+++ app/Repositories/PermissionRepository.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\Repositories;
+
+use App\Models\Permission;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+
+class PermissionRepository
+{
+    protected $model;
+
+    public function __construct(Permission $permission)
+    {
+        $this->model = $permission;
+    }
+    public function all()
+    {
+        return $this->model->all();
+    }
+    public function paginate(int $perPage = 10): LengthAwarePaginator
+    {
+        return $this->model->paginate(perPage: $perPage);
+    }
+    public function findById(int $id): Permission
+    {
+        return $this->model->findOrFail(id: $id);
+    }
+    public function create(array $data): Permission
+    {
+        return $this->model->create(attributes: $data);
+    }
+    public function update(int $id, array $data): Permission
+    {
+        $permission = $this->findById(id: $id);
+        $permission->update(attributes: $data);
+        return $permission;
+    }
+    public function delete(int $id): bool
+    {
+        $permission = $this->findById(id: $id);
+        return $permission->delete();
+    }
+}
---
+++ app/Repositories/ProfileRepository.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\Repositories;
+
+use App\Models\Profile;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+
+class ProfileRepository
+{
+    protected $model;
+
+    public function __construct(Profile $profile)
+    {
+        $this->model = $profile;
+    }
+    public function all()
+    {
+        return $this->model->all();
+    }
+    public function paginate(int $perPage = 10): LengthAwarePaginator
+    {
+        return $this->model->paginate(perPage: $perPage);
+    }
+    public function findById(int $id): Profile
+    {
+        return $this->model->findOrFail(id: $id);
+    }
+    public function create(array $data): Profile
+    {
+        return $this->model->create(attributes: $data);
+    }
+    public function update(int $id, array $data): Profile
+    {
+        $profile = $this->findById(id: $id);
+        $profile->update(attributes: $data);
+        return $profile;
+    }
+    public function delete(int $id): bool
+    {
+        $profile = $this->findById(id: $id);
+        return $profile->delete();
+    }
+}
