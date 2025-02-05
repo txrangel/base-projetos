b96bf64cd3a5c0162b44341a208d6db37f445d24
txrangel <joaovitorrtd@gmail.com>
2025-02-05 13:22:56 -0300

    feat: adjusting routes to have profile and permissions functionalities with middleware

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,63 @@
 <?php
 
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserProfileController;
+use App\Http\Middleware\CheckPermission;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::view(uri: '/', view: 'dashboard')->name(name: 'dashboard');
+
+    Route::prefix('profile')->name('profile.')->controller(ProfileController::class)->group(function () {
+        Route::get('/', 'index')
+            ->name('index')
+            ->middleware(CheckPermission::class . ':profile.index');
+        Route::get('/create', 'create')
+            ->name('create')
+            ->middleware(CheckPermission::class . ':profile.create');
+        Route::post('/store', 'store')
+            ->name('store')
+            ->middleware(CheckPermission::class . ':profile.store');
+        Route::get('/{id}/edit', 'edit')
+            ->name('edit')
+            ->middleware(CheckPermission::class . ':profile.edit');
+        Route::put('/{id}/update', 'update')
+            ->name('update')
+            ->middleware(CheckPermission::class . ':profile.update');
+        Route::delete('/', 'destroy')
+            ->name('destroy')
+            ->middleware(CheckPermission::class . ':profile.destroy');
+    });
+
+    Route::prefix('permission')->name('permission.')->controller(PermissionController::class)->group(function () {
+        Route::get('/', 'index')
+            ->name('index')
+            ->middleware(CheckPermission::class . ':permission.index');
+        Route::get('/create', 'create')
+            ->name('create')
+            ->middleware(CheckPermission::class . ':permission.create');
+        Route::post('/store', 'store')
+            ->name('store')
+            ->middleware(CheckPermission::class . ':permission.store');
+        Route::get('/{id}/edit', 'edit')
+            ->name('edit')
+            ->middleware(CheckPermission::class . ':permission.edit');
+        Route::put('/{id}/update', 'update')
+            ->name('update')
+            ->middleware(CheckPermission::class . ':permission.update');
+        Route::delete('/', 'destroy')
+            ->name('destroy')
+            ->middleware(CheckPermission::class . ':permission.destroy');
+    });
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [UserProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [UserProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [UserProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::prefix('user/profile')->name('user.profile.')->controller(UserProfileController::class)->group(function () {
+        Route::get('/', 'edit')->name('edit');
+        Route::patch('/', 'update')->name('update');
+        Route::delete('/', 'destroy')->name('destroy');
+    });
 });
 
 require __DIR__.'/auth.php';
