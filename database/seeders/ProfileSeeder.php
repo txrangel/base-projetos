fd5855807e0e1a001803b25510d395cb7964502d
txrangel <joaovitorrtd@gmail.com>
2025-02-05 16:11:06 -0300

    refacotr/feat: adjusting seeders

--- database/seeders/ProfileSeeder.php
+++ database/seeders/ProfileSeeder.php
@@ -14,6 +14,7 @@ class ProfileSeeder extends Seeder
     public function run()
     {
         Profile::create(['name' => 'Admin']);
-        Profile::create(['name' => 'User']);
+        Profile::create(['name' => 'Editor']);
+        Profile::create(['name' => 'Viewer']);
     }
 }
