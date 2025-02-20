7536d554b382527d71508951140a4b829e607359
txrangel <joaovitorrtd@gmail.com>
2025-02-20 12:53:11 -0300

    chore: adicionando filament | refactor: mudando todo o projeto para filament

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,4 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
+    App\Providers\Filament\AdminPanelPanelProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
 ];
